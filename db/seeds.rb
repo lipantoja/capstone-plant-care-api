284f9c7d3e8f936047d8d179315cf39638d8f57c
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-11-06 21:35:45 -0500

    added model for plant and user, seeds for plant data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+
+# plant = Plant.new(
+#   name: "Begonias",
+#   description: "Leafy plant that originated in India.",
+#   amount_of_sun: 6,
+#   days_to_water: 4,
+# )
+# plant.save
+
+# user = User.new(
+#   name: "",
+#   email: "",
+#   password_digest: "password",
+#   image_url: "",
+# )
+# user.save
